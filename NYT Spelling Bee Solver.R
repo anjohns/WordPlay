6d1bb01baec966a4cbf061ae3e55108664f1ae07
Andrew Johnson <agjohns2@ncsu.edu>
2018-09-10 11:31:55 -0400

    added comments STILL NEEDED    unique word collector should be able to ignore non txt file types

--- NYT Spelling Bee Solver.R
+++ NYT Spelling Bee Solver.R
@@ -5,54 +5,70 @@
 
 ########################################################################################################
 ###########   ENTER LETTERS AND I WILL GIVE YOU EVERY WORD COMPRISED OF ONLY THOSE LETTERS  ############
-########################################################################################################
-keyLetters <- "hbgutor"
-focalLetter <- 'r'
+#####################  ENTER A FOCAL LETTER AND EVERY WORD WILL CONTAIN THAT LETTER   ##################
+keyLetters <- "hndiarb"
+focalLetter <- 'b'
 
 minimumWordLength <- 4
 
 ########################################################################################################
 ########################################################################################################
-########################################################################################################
+
+# generates a full alphabet vector
 alphabet <- c('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v',
               'w','x','y','z')
 
+# variable for number of key letters
 keyLetterLength <- nchar(keyLetters)
+
+# variable for the number of letters not included in the key letters (# of total letters - # of key letters )
 complementLength <- length(alphabet) - keyLetterLength
 
-funcLeters <- unlist(strsplit(keyLetters, split=""))
+# separates the string of user input (i.e. letters of interest) into individual characters
+funcLetters <- unlist(strsplit(keyLetters, split=""))
 
+# initially just a copy of the alphabet vector
 keyLetterComplement <- alphabet
 
+# removes each key letter from the alphabet set, producing the complement of the key letter set
 for(i in 1:keyLetterLength){
-  keyLetterComplement <- keyLetterComplement[!(grepl(funcLeters[i], keyLetterComplement))]
+  keyLetterComplement <- keyLetterComplement[!(grepl(funcLetters[i], keyLetterComplement))]
 }
 
 ########################################################################################################
 #   import dataset of words and convert it to a vector
 ########################################################################################################
 
-textDataAlpha <- read.table("C:/Users/Andrew/Desktop/R Data Files/AndrewsWordBank.txt")
+# imports a text bank to reference
+textDataAlpha <- read.table("C:/Users/Andrew/Desktop/R Data Files/Word Bank Alpha.txt")
 
+# produces a vector with a string as each element
 textVector <- as.vector(textDataAlpha$V1)
 
+# removes all words smaller than a predetermined length from the word bank
 textData <- textVector[nchar(textVector) >= minimumWordLength]
 
+# removes all duplicated words from the word bank
 textData <- unique(textData)
 
 ########################################################################################################
-########################################################################################################
 
-
+# creates a variable to manipulate the original text bank
 textOutput <- textData
 
+# removes all words that contain each letter of the complement set from textOutput
 for(j in 1:complementLength){
+  
   textOutput <- textOutput[!(grepl(keyLetterComplement[j], textOutput, ignore.case = TRUE))]
+  
 }
 
+# removes all words that do not contain the focal letter from textOutput 
 textOutput <- textOutput[grepl((focalLetter), textOutput, ignore.case = TRUE)]
 
+# orders textOutput vector by word length in decending order
 textOutput <- textOutput[order(nchar(textOutput), textOutput, decreasing = TRUE)]
 
+# prints textOutput to the consol (hopefully to be used to solve the NYT Spelling Bee puzzle)
 textOutput
 
--- Unique Word Collector.R
+++ Unique Word Collector.R
@@ -6,10 +6,14 @@
 ########################################################################################################
 ########################################################################################################
 
+# sets working directory
 setwd("C:/Users/Andrew/Desktop/R Data Files/")
 
+# function that receives a file name (should be a text file) and returns a vector of unique words from
+# that file
 WordCollector <- function(fileName){
-  # imports text file
+  
+  # reads text file
   textFile <- readLines(fileName, encoding = "UTF-8")
   
   # parses text into individual strings
@@ -38,17 +42,31 @@ WordCollector <- function(fileName){
   textFile8 <- textFile7[!(grepl("[-]?[0-9]+[.]?[0-9]*|[-]?[0-9]+[L]?|[-]?[0-9]+[.]?[0-9]*[eE][0-9]+",
                                  textFile7))]
   
+  # returns final text vector
   return(textFile8)
   
 }
 
+# produces a vector of all the file names in the working directory
 nameVector <- c(list.files(pattern = ".txt"))
 
+# produces a blank vector intended to take on a new unique word vector from each file
 TotalSet <- c()
+
+# this loop cycles through the working directory, reading each file and adding the new vector of 
+# unique words to the TotalSet vector
 for(i in 1:length(nameVector)){
+  
+  # sends a file name to the WordCollector function 
   uniqueWords <- WordCollector(nameVector[i])
+  
+  # adds the new vector to the primary vector, TotalSet
   TotalSet <- c(TotalSet, uniqueWords)
 }
 
+# removes all repeats from TotalSet
 TotalSet <- unique(TotalSet)
+
+# used to print the new word bank to the working directory
+#write(TotalSet, file = "Word Bank.txt")
 
